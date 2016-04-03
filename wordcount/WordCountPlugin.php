d94eb6c6e867d86c62f8743d34a24c57e69e6c6e
Nathan Reed <info@vimia.co.uk>
2016-04-03 14:18:20 +0100

    Updated plugin details to include releases

--- wordcount/WordCountPlugin.php
+++ wordcount/WordCountPlugin.php
@@ -1,31 +1,61 @@
 <?php
 namespace Craft;
 class WordCountPlugin extends BasePlugin
-{
+{   
+    protected $_version = '1.0.0',
+      $_schemaVersion = '1.0.0',
+      $_name = 'Word Count',
+      $_url = 'https://github.com/zizther/craft_wordcount',
+      $_releaseFeedUrl = 'https://raw.githubusercontent.com/zizther/craft_wordcount/master/releases.json',
+      $_documentationUrl = 'https://github.com/zizther/craft_wordcount/blob/master/README.md',
+      $_description = 'An accurate word count Twig filter for Craft CMS.',
+      $_developer = 'Nathan Reed',
+      $_developerUrl = 'http://vimia.co.uk/',
+      $_minVersion = '2.0';
+    
     public function getName()
     {
-         return Craft::t('Word Count');
+        return Craft::t($this->_name);
+    }
+    public function getUrl()
+    {
+        return $this->_url;
     }
     public function getVersion()
     {
-        return '1.0.0';
+        return $this->_version;
     }
     public function getDeveloper()
     {
-        return 'Vimia';
+        return $this->_developer;
     }
     public function getDeveloperUrl()
     {
-        return 'http://www.vimia.co.uk';
+        return $this->_developerUrl;
     }
-    public function getPluginUrl()
+    public function getDescription()
     {
-        return 'https://github.com/zizther/craft_wordcount';
+        return $this->_description;
     }
     public function getDocumentationUrl()
     {
-        return $this->getPluginUrl() . '/blob/master/README.md';
+        return $this->_documentationUrl;
     }
+    public function getSchemaVersion()
+    {
+        return $this->_schemaVersion;
+    }
+    public function getReleaseFeedUrl()
+    {
+        return $this->_releaseFeedUrl;
+    }
+    public function getCraftRequiredVersion()
+    {
+        return $this->_minVersion;
+    }
+    
+    
+    // Twig Extension
     public function addTwigExtension()  
     {
         Craft::import('plugins.wordcount.twigextensions.WordCountTwigExtension');
